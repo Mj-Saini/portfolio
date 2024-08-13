ea20a2563be08b38d9b9e1510359dd0168fce251
Mj-Saini <manojsaini97285@gmail.com>
2024-08-13 10:26:08 +0530

    add font family

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-      <div>
-          
-    </div>
-  )
-}
+    <>
+      <div className="container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5">
+        <div className="flex">
+          <h1 className="text-white text-6xl"> helloworld</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
