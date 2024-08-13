599a8d213c37e80e32e7bb2ad25bebde96bf94f5
Mj-Saini <manojsaini97285@gmail.com>
2024-08-13 11:57:10 +0530

    icon jsx

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -5,7 +5,16 @@ const Hero = () => {
     <>
       <div className="container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5">
         <div className="flex">
-          <h1 className="text-white text-6xl"> helloworld</h1>
+          <div className="w-full lg:w-1/2">
+            <div>
+              <p>Full Stack Developer</p>
+              <h1>Hello I’ m Vikas Boora</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. At augue dignissim eu
+                posuere non mi dictum lacus praesent. Aliquet elit r dolor.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
