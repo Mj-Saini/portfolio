56e612c3916387b665cf55bcacfd42a7bb485468
Ankita <sehrawatankita1@gmail.com>
2024-08-17 14:02:09 +0530

    heroSecUpdate

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -3,102 +3,103 @@ import React, { useState } from 'react'
 
 const Header = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
-    if(toggleMenu){
-        document.body.style.overflow="hidden"
-    }else{
-        document.body.style.overflow="auto"
+    if (toggleMenu) {
+        document.body.style.overflow = "hidden"
+    } else {
+        document.body.style.overflow = "auto"
 
     }
 
-  return (
-    <section>
-      <div className="container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5">
-        <div className="pt-[34px] pb-9 flex items-center justify-between">
-          <div className="md:w-3/12 min-[1130px]:w-2/12">
-            <a className="text-[32px] font-normal text-white " href="">
-              Vikas Boora
-            </a>
-          </div>
+    return (
+        <section>
+            <div className="container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5">
+                <div className="pt-[34px] pb-9 flex items-center justify-between">
+                    <div className="md:w-3/12 min-[1130px]:w-2/12">
+                        <a className="text-[32px] font-normal text-white " href="">
+                            Vikas Boora
+                        </a>
+                    </div>
 
-          <div
-            className={` md:w-9/12 min-[1130px]:w-7/12 max-md:h-screen max-md:fixed top-0  max-md:w-screen max-md:bg-black max-md:z-10 max-md:flex items-center justify-center duration-300 ${
-              toggleMenu ? "left-0" : "-left-full"
-            }`}
-          >
-            <ol className="flex flex-col md:flex-row items-center gap-6 lg:gap-[50px] justify-end  ">
-              <li>
-                <a
-                  className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full after:hover:left-0 duration-300 after:duration-300"
-                  href="home"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-base font-normal text-white hover:text-[#15FFEA] hover:border-b hover:border-[#15FFEA]  "
-                  href=""
-                >
-                  Service
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-base font-normal text-white hover:text-[#15FFEA] hover:border-b hover:border-[#15FFEA]  "
-                  href=""
-                >
-                  Resume
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-base font-normal text-white hover:text-[#15FFEA] hover:border-b hover:border-[#15FFEA]  "
-                  href=""
-                >
-                  Work
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-base font-normal text-white hover:text-[#15FFEA] hover:border-b hover:border-[#15FFEA]  "
-                  href=""
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <button className="text-base font-normal text-black    bg-[#15FFEA] p-[10px_20px] rounded-[50px] ">
-                  Hire me
-                </button>
-              </li>
-            </ol>
-          </div>
+                    <div
+                        className={` md:w-9/12 min-[1130px]:w-7/12 max-md:h-screen max-md:fixed top-0  max-md:w-screen max-md:bg-black max-md:z-10 max-md:flex items-center justify-center duration-300 ${toggleMenu ? "left-0" : "-left-full"
+                            }`}
+                    >
+                        <ol className="flex flex-col md:flex-row items-center gap-6 lg:gap-[50px] justify-end  ">
+                            <li>
+                                <a
+                                    onClick={() => setToggleMenu(false)}
+                                    className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full hover:text-[#15FFEA] after:hover:left-0 duration-300 after:duration-300"
+                                    href="#home "
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    onClick={() => setToggleMenu(false)}
+                                    className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full hover:text-[#15FFEA] after:hover:left-0 duration-300 after:duration-300 "
+                                    href="#service"
+                                >
+                                    Service
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    onClick={() => setToggleMenu(false)}
+                                    className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full hover:text-[#15FFEA] after:hover:left-0 duration-300 after:duration-300 "
+                                    href=""
+                                >
+                                    Resume
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    onClick={() => setToggleMenu(false)}
+                                    className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full hover:text-[#15FFEA] after:hover:left-0 duration-300 after:duration-300 "
+                                    href=""
+                                >
+                                    Work
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    onClick={() => setToggleMenu(false)}
+                                    className="text-base font-normal text-white relative after:absolute after:w-0 after:h-0.5 after:bg-[#15FFEA] after:-bottom-1 after:left-1/2 after:hover:w-full hover:text-[#15FFEA] after:hover:left-0 duration-300 after:duration-300 "
+                                    href=""
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                            <li>
+                                <button className="text-base font-normal text-[#15FFEA] border border-[#15FFEA] p-[10px_20px] rounded-[50px]  duration-300 hover:duration-300 hover:bg-[#15FFEA] hover:text-black ">
+                                    Hire me
+                                </button>
+                            </li>
+                        </ol>
+                    </div>
 
-          {/* transition-transform */}
-          <div
-            onClick={() => setToggleMenu(!toggleMenu)}
-            className="w-6 h-5 flex flex-col justify-between duration-300 md:hidden z-[11] cursor-pointer"
-          >
-            <span
-              className={`w-full border-[2px] border-white duration-300 bg-white ${
-                toggleMenu ? "rotate-45 translate-y-[8.5px]" : ""
-              }`}
-            ></span>
-            <span
-              className={`w-full border-[2px] border-white duration-300 bg-white ${
-                toggleMenu ? "-rotate-45" : ""
-              }`}
-            ></span>
-            <span
-              className={`w-full border-[2px] border-white duration-300 bg-white ${
-                toggleMenu ? "opacity-0" : ""
-              }`}
-            ></span>
-          </div>
-        </div>
-        </div>
 
-                   
+                    <div
+                        onClick={() => setToggleMenu(!toggleMenu)}
+                        className="w-6 h-5 flex flex-col justify-between duration-300 md:hidden z-[11] cursor-pointer"
+                    >
+                        <span
+                            className={`w-full border-[2px] border-white duration-300 bg-white ${toggleMenu ? "rotate-45 translate-y-[8.5px]" : ""
+                                }`}
+                        ></span>
+                        <span
+                            className={`w-full border-[2px] border-white duration-300 bg-white ${toggleMenu ? "-rotate-45" : ""
+                                }`}
+                        ></span>
+                        <span
+                            className={`w-full border-[2px] border-white duration-300 bg-white ${toggleMenu ? "opacity-0" : ""
+                                }`}
+                        ></span>
+                    </div>
+                </div>
+            </div>
+
+
         </section >
     )
 }
--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -4,6 +4,7 @@ import vikash_img from "../../../public/assets/images/png/Vikash_img.png";
 import {
   DownloadIcon,
   GithubIcon,
+  HeroSecElp,
   LinkdinIcon,
   TwitterIcon,
   YouTubeIcon,
@@ -12,7 +13,7 @@ import { HeroSecArr } from "./Helper";
 
 const Hero = () => {
   return (
-    <div className="h-screen md:h-[calc(100vh-118px)] flex flex-col">
+    <div className="min-h-[calc(100vh-118px)] flex flex-col">
       <div className="container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5  flex-grow flex flex-col justify-center">
         <div className="flex flex-wrap items-center flex-col-reverse md:flex-row w-full">
           <div className="w-full mt-4 md:mt-0 md:w-1/2 text-center sm:text-start ">
@@ -23,43 +24,48 @@ const Hero = () => {
               <h1 className="font-semibold text-4xl md:text-[54px] lg:text-7xl xl:text-[80px] text-white md:!leading-[50px] lg:!leading-[90px] mt-1">
                 Hello I’ m <span className="text-[#15FFEA]">Navdeep singh</span>
               </h1>
-              <p className="font-normal text-base md:text-lg text-white/80 py-4 md:py-[30px]">
+              <p className="font-normal text-base md:text-lg text-white/80 pt-4 pb-10 md:py-[30px]">
                 Lorem ipsum dolor sit amet consectetur. At augue dignissim eu
                 posuere non mi dictum lacus praesent. Aliquet elit r dolor.
               </p>
             </div>
             <div className="flex max-[420px]:flex-col gap-5 lg:gap-[29px] justify-center sm:justify-start  ">
-              <button className=" border border-[#15FFEA] p-[10px] min-[850px]:p-[10px_20px] flex items-center justify-center gap-1 min-[850px]:gap-[10px] rounded-[50px] max-sm:w-[200px] max-[460px]:m-auto ">
-                <p className="text-center uppercase text-[#15FFEA] text-sm min-[850px]:text-base ">
+              <button className=" border border-[#15FFEA] p-[10px] min-[850px]:p-[10px_20px] flex items-center justify-center gap-1 min-[850px]:gap-[10px] rounded-[50px] max-sm:w-[200px] max-[460px]:m-auto duration-300 hover:duration-300 hover:bg-[#15FFEA]  group ">
+                <p className="text-center uppercase text-[#15FFEA] text-sm min-[850px]:text-base group-hover:text-black ">
                   Download cv
                 </p>
-                <DownloadIcon />{" "}
+                <DownloadIcon />
               </button>
               <div className="flex max-sm:justify-center gap-2 lg:gap-5">
                 <a target="_blank" href="https://www.github.com/">
-                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10">
+                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10 duration-500 hover:-translate-y-3 hover:bg-[#15FFEA] socialHover ">
                     <GithubIcon />
                   </span>
                 </a>
                 <a target="_blank" href="https://www.github.com/">
-                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10">
+                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10 duration-500 hover:-translate-y-3 hover:bg-[#15FFEA] socialHover ">
                     <YouTubeIcon />
                   </span>
                 </a>
                 <a target="_blank" href="https://www.github.com/">
-                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10">
+                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10 duration-500 hover:-translate-y-3 hover:bg-[#15FFEA] socialHover ">
                     <TwitterIcon />
                   </span>
                 </a>
                 <a target="_blank" href="https://www.github.com/">
-                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10">
+                  <span className="border border-[#15FFEA] rounded-[50px]  flex justify-center items-center w-10 h-10 duration-500 hover:-translate-y-3 hover:bg-[#15FFEA] socialHover ">
                     <LinkdinIcon />
                   </span>
                 </a>
               </div>
             </div>
           </div>
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <div className="w-full md:w-1/2 flex justify-center md:justify-end relative">
+
+            <div className="absolute left-0 lg:-top-12 xl:-top-10 md:-top-16 sm:-top-5 hidden sm:block ">
+              <HeroSecElp />
+            </div>
+
             <div className=" after:absolute max-[400px]:w-[280px] max-[400px]:h-[280px] w-[360px] h-[360px] lg:w-[488px] lg:h-[488px] relative after:w-full after:h-full after:animate-spin after:duration-[20s]  rounded-[700px] after:rounded-[700px] after:border-[4px] after:border-dashed after:border-[#15FFEA] after:shadow-[-5px_5px_40px_0px_#15FFEA66]  after:top-0 after:left-0 overflow-hidden">
               <Image
                 className=" w-[344px] h-[478px] object-cover object-center m-auto "
@@ -69,18 +75,21 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-[90%] mx-auto flex flex-wrap gap-5 md:gap-16  lg:gap-[100px] justify-center sm:justify-between md:justify-center mt-[70px]">
+
+
+        {/* ### anylytics ### */}
+        <div className="w-full lg:w-[90%] mx-auto flex flex-col min-[400px]:flex-row flex-wrap gap-5 md:gap-16  lg:gap-[100px] items-center justify-center sm:justify-between md:justify-center mt-[70px] mb-8 ">
           {HeroSecArr.map((item, i) => {
             return (
               <div
                 key={i}
-                className="max-sm:w-4/12 w-2/12 flex gap-[15px] items-center"
+                className=" max-[400px]:w-7/12 min-[400px]:w-4/12 sm:w-2/12 flex gap-[15px] items-center"
               >
-                <h1 className=" ff_roadRage text-5xl lg:text-[64px] font-normal text-[#FFFFFFCC] ">
+                <h1 className="max-[400px]:w-6/12 ff_roadRage text-5xl lg:text-[64px] font-normal text-[#FFFFFFCC] ">
                   {" "}
                   {item.numVal}{" "}
                 </h1>
-                <p className="text-sm lg:text-lg font-normal text-[#FFFFFFCC] ">
+                <p className="max-[400px]:w-6/12  text-sm lg:text-lg font-normal text-[#FFFFFFCC] ">
                   {" "}
                   {item.text}{" "}
                 </p>
