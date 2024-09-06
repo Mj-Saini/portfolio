b7d6a94e2c1d83a3bf6af3165cfaabb4101ce7b6
Ankita <sehrawatankita1@gmail.com>
2024-09-06 10:37:21 +0530

    footer lines

--- src/app/Components/Connect.jsx
+++ src/app/Components/Connect.jsx
@@ -4,8 +4,8 @@ const Connect = () => {
     return (
         <div id='contact' className='relative max-lg:py-12 pt-[78px] pb-[91px] '>
             <div className='container max-w-[1140px] 2xl:max-w-[1550px] mx-auto px-5  '>
-                <h1 className='max-[400px]:text-[50px] text-[60px] lg:text-[70px] font-normal text-white sm:mb-5 lg:mb-0  '>Let's Connect!</h1>
-
+                <h1 className='max-[400px]:text-[50px] text-[60px] lg:text-[70px] font-normal text-white mb-[18px]   '>Let's Connect!</h1>
+                <p className='text-lg font-normal text-[#FFFFFFCC] lg:w-[663px] '>Feel free to reach out for collaborations, projects, or just to say hi. I'm always open to new opportunities and interesting conversations. Drop me a message, and I'll get back to you as soon as possible!</p>
             </div>
         </div>
     )
