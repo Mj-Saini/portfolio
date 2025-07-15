09741d76b906ddfef9119fcf73d96fbb2cb01417
mj-saini <manojsaini97285@gmail.com>
2025-07-15 20:44:03 +0530

    add resume

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
               </p>
             </div>
             <div className="flex max-[420px]:flex-col gap-5 lg:gap-[29px] justify-center sm:justify-start  ">
-              <Link target="_blank" href="/assets/pdf/Manoj-resume.pdf" download className=" border border-[#15FFEA] p-[10px] min-[850px]:p-[10px_20px] flex items-center justify-center gap-1 min-[850px]:gap-[10px] rounded-[50px] max-sm:w-[200px] max-[460px]:m-auto duration-300 hover:duration-300 hover:bg-[#15FFEA]  group ">
+              <Link target="_blank" href="/assets/pdf/its-my-resume.pdf" download className=" border border-[#15FFEA] p-[10px] min-[850px]:p-[10px_20px] flex items-center justify-center gap-1 min-[850px]:gap-[10px] rounded-[50px] max-sm:w-[200px] max-[460px]:m-auto duration-300 hover:duration-300 hover:bg-[#15FFEA]  group ">
                 <p className="text-center uppercase text-[#15FFEA] text-sm min-[850px]:text-base group-hover:text-black ">
                   Download cv
                 </p>
